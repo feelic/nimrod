0463727b3d00eb968a138434324c87c26ef8310e
feelic <feelics@gmail.com>
2016-07-15 15:39:34 +0200

    feat(premadeLanguages): refine rules for premade french language

--- languages/french.js
+++ languages/french.js
@@ -1,13 +1,13 @@
 const Language = require('../lib/language.js');
 const options = {
   'vowels': ['i', 'y', 'u', 'e', 'ø', 'ə', 'o', 'ɛ', 'œ', 'ɔ', 'ɛ̃', 'œ̃', 'ɔ̃', 'ɑ̃', 'a', 'ɑ'],
-  'consonants': ['m', 'n', 'ɲ', 'p', 't', 'k', 'b', 'd', 'g', 'f', 's', 'ʃ', 'v', 'z', 'ʒ', 'ʁ', 'l', 'j', 'ɥ', 'w'],
+  'consonants': ['m', 'n', 'ɲ', 'p', 't', 'k', 'b', 'd', 'g', 'f', 's', 'ʃ', 'v', 'z', 'ʒ', 'ʁ', 'l'],
   'syllableRules': {
     'onset': {
       'existence': () => true && Math.round(Math.random()),
       'phonemes': [
-        'm', 'n', 'ɲ', 'p', 't', 'k', 'b', 'd', 'g', 'f', 's', 'ʃ', 'v', 'z', 'ʒ', 'ʁ', 'l', 'j', 'w',
-        'pl', 'kl', 'bl', 'gl', 'fl', 'sl', 'ʃl', 'vl',
+        'm', 'n', 'ɲ', 'p', 't', 'k', 'b', 'd', 'g', 'f', 's', 'ʃ', 'v', 'z', 'ʒ', 'ʁ', 'l',
+        'pl', 'kl', 'bl', 'gl', 'fl', 'sl', 'ʃl',
         'pʁ', 'kʁ', 'tʁ', 'bʁ', 'dʁ', 'gʁ', 'fʁ', 'vʁ',
         'nɥ', 'pɥ', 'lɥ',
         'pt',
@@ -16,12 +16,12 @@ const options = {
       ]
     },
     'nucleus': {
-      'diphthong': () => true && Math.round(Math.random() * Math.random()),
+      'diphthong': () => false,
       'phonemes': ['i', 'y', 'u', 'e', 'ø', 'ə', 'o', 'ɛ', 'œ', 'ɔ', 'ɛ̃', 'œ̃', 'ɔ̃', 'ɑ̃', 'a', 'ɑ']
     },
     'coda': {
-      'existence': () => true && Math.round(Math.random()),
-      'phonemes': ['m', 'n', 'ɲ', 'p', 't', 'k', 'b', 'd', 'g', 'f', 's', 'ʃ', 'v', 'z', 'ʒ', 'ʁ', 'l']
+      'existence': () => true && Math.round(Math.random() * Math.random()),
+      'phonemes': ['m', 'n', 'p', 't', 'k', 'd', 'g', 'f', 's', 'ʃ', 'v', 'z', 'ʁ', 'l']
     }
   },
   'transliteration': {
